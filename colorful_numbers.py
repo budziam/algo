ce71ea1e767ce51cc373d7e599ec5716e738cd18
Michał Budziak <michal.mariusz.b@gmail.com>
2020-02-26 20:03:33 +0100

    Do not use text

--- colorful_numbers.py
+++ colorful_numbers.py
@@ -1,22 +1,31 @@
 # https://algorithms.tutorialhorizon.com/colorful-numbers/
+from math import log10
 
 
 def is_colorful(number: int) -> bool:
-    text = str(number)
-
+    number = abs(number)
     existing = set()
+    length = int(log10(number)) + 1 if number else 1
 
-    for i in range(len(text)):
-        my_sum = 1
-        for char in text[i:]:
-            my_sum *= int(char)
+    size = 10 ** length
+    while size > 1:
+        current_number = number % size
+        result = 1
 
-            if my_sum in existing:
+        size_for_iteration = size
+        while size_for_iteration > 1:
+            current_digit = (current_number % size_for_iteration) // (size_for_iteration / 10)
+            result *= current_digit
+
+            if result in existing:
                 return False
 
-            existing.add(my_sum)
+            existing.add(result)
+            size_for_iteration //= 10
+
+        size //= 10
 
     return True
 
 
-print(is_colorful(324))
+print(is_colorful(3245))
